c268661038c76307b120a9138e0d37f06bc2e0d6
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-14 01:31:54 +0200

    commit

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const inputE = document.getElementById("input-e")
 const inputBtn = document.getElementById("input-btn")
 const ulE = document.getElementById("ul-e")
 
+let leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocal) {
+    myLeads = leadsFromLocal
+    renderLeads()
+}
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputE.value)
     inputE.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
 function renderLeads() {
-
-}
-for (let i = 0; i < myLeads.length; i++) {
     let listItems = ""
-    listItems += `
-        <li>
-            <a target="_blank" href="${myLeads[i]}">
-                ${myLeads[i]}
-            </a>
-        </li>
-    `
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target="_blank" href="${myLeads[i]}">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulE.innerHTML = listItems
 }
-ulE.innerHTML = listItems
