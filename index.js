1cacdb3614a46c41f8aec820a9da4239b38066e7
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-13 23:55:16 +0200

    commit

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const inputE = document.getElementById("input-e")
 const inputBtn = document.getElementById("input-btn")
 const ulE = document.getElementById("ul-e")
 
-inputBtn.addEventListener("click", function btnClicked() {
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputE.value)
-    console.log(myLeads)
+    inputE.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulE.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+
 }
+for (let i = 0; i < myLeads.length; i++) {
+    let listItems = ""
+    listItems += `
+        <li>
+            <a target="_blank" href="${myLeads[i]}">
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+}
+ulE.innerHTML = listItems
