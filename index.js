728db2a5d2c9d21783bb2ffc0245bc62a6c85454
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-15 16:45:28 +0200

    commit

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ const tabs = [
 ]
 
 tabBtn.addEventListener("clicks", function() {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
 
-    render(myLeads)
+        render(myLeads)
+    })
 })
 
 inputBtn.addEventListener("click", function() {
